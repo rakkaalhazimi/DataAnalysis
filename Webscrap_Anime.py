dbbbaf83dc9b6f5dc2a820532405857216b5608e
Rakka Alhazimi <58966937+rakkaalhazimi@users.noreply.github.com>
2020-03-12 17:24:17 +0700

    Update Webscrap_Anime.py

--- Webscrap_Anime.py
+++ Webscrap_Anime.py
@@ -1,22 +1,28 @@
-from tkinter import *                                              # tkinter for GUI
-import requests, bs4                                               # request url and bs4 for html parsing
-import webbrowser                                                  # open default web browser
+"""
+This is my simple project to scrap anime names into a list and embed its url links.
+You can double-click the anime name to redirect your web browser to its url.
+How this program work:
+1. First it create list-box GUI for anime names container
+2. Then, parse html from specific website (not stable)
+3. Html parser produce two result: anime names and its urls and pass it to the GUI
 
-#webbrowser.open('https://anoboy.video/', new=1)
+Any suggestion please consider email to rakkakeren@gmail.com, cc: project name
+"""
 
-# The main objective of this project:
-# 1. Get the on-going anime schedule in GUI
-# 2. Open the website based on anime title
+from tkinter import *
+import requests, bs4
+import webbrowser
 
 
-class AnimeListbox(Frame):                                          # Make a class that inherite the tkinter Frame
-    def __init__(self, animelist, urllist=None, parent=None):       # This is what will the class do when instantiated
+class AnimeListbox(Frame):
+    """Set up list-box GUI that contains anime names"""
+    def __init__(self, animelist, urllist=None, parent=None):
         Frame.__init__(self, parent)
         self.pack(expand=YES, fill=BOTH)
         self.urlist = urllist
         self.makeListBox(animelist)
 
-    def makeListBox(self, animelist):                               # Make GUI widgets contain anime titles
+    def makeListBox(self, animelist):
         sbar = Scrollbar(self)
         list = Listbox(self, width=100, relief=SUNKEN)
         sbar.config(command=list.yview)
@@ -27,39 +33,40 @@ class AnimeListbox(Frame):                                          # Make a cla
         for index, anime in enumerate(animelist):
             list.insert(index, anime)
         list.config(selectmode=SINGLE, setgrid=1)
-        list.bind('<Double-1>', self.selectAnime)                   # Bind the anime titles with double-click command
+        list.bind('<Double-1>', self.selectAnime)
         self.animelist = list
 
-    def selectAnime(self, event):                                   # Determine what event will occur when the user
-        index = self.animelist.curselection() # double click        # double click the anime titles
+    def selectAnime(self, event):
+        index = self.animelist.curselection() # double click
         index = index[0]
         self.openBrowser(self.urlist[index])
 
-    def openBrowser(self, url):                                     # It will open your web browser and redirect you to
-        webbrowser.open(url, new=1)                                 # your selected anime website.
+    def openBrowser(self, url):
+        webbrowser.open(url, new=1)
 
 
-def htmlParser():                                                   # Parse the website html code
-    url = 'https://anoboy.video'                                    # Target: anoboy.video
+def htmlParser():
+    """Parse html on website (website dependent)"""
+    url = 'https://otakudesu.org/'
     link = requests.get(url)
     soup = bs4.BeautifulSoup(link.content, 'html.parser')
 
-    animeminer = soup.find_all('div', {'class':'amvj'})             # Find all anime titles
+    animeminer = soup.find_all('h2', {'class':'jdlflm'})
     animelist = []
     for anime in animeminer:
         animelist.append(anime.get_text())
 
-    urlminer = soup.find_all('a', {'title':[animelist]})            # Find all anime titles urls
+    urlminer = soup.find_all('a')
     urlist = []
     for url in urlminer:
         x = url.get('href')
         urlist.append(x)
 
-    return animelist, urlist
+    return animelist[:10], urlist[12:22]
 
-if __name__ == '__main__':                                          # Code execution
+if __name__ == '__main__':
     win = Tk()
-    win.title('Anime List Schedule 2020')
-    Label(win, text='Source: Anoboy.Video').pack()
+    win.title('List of On-going Anime Schedule in 2020')
+    Label(win, text='Source: Otakudesu.org').pack()
     animelist, urlist = htmlParser()
     AnimeListbox(animelist, urlist, parent=win).mainloop()
